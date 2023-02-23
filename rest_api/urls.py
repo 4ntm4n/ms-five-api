9a0818563754d2e340b287ba93c6a3d26bfc8d20
Anton Askling <anton.askling@gmail.com>
2023-02-23 23:22:24 +0100

    add a root route for hte api

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import root_route
 
 urlpatterns = [
+    path('', root_route),
     path('admin/', admin.site.urls),
     
     #session login from django rest framework
-    path('api-auth/', include('rest_framework.url'))
+    path('api-auth/', include('rest_framework.urls')),
 ]
