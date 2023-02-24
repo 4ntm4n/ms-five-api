dbb7b8941ee8fdb7647caa39b7b60396344d286f
Anton Askling <anton.askling@gmail.com>
2023-02-24 01:42:04 +0100

    add profile details view and update urls.py

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('profiles/', views.ProfileListView.as_view()),
+    path('profiles/<int:pk>/', views.ProfileDetailView.as_view()),
+
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,15 @@
 from rest_framework import generics
 from .models import Profile
 from .serializers import ProfileSerializer
+from rest_api.permissions import IsOwnerOrReadOnly
 
 class ProfileListView(generics.ListAPIView):
     serializer_class = ProfileSerializer
     queryset = Profile.objects.all(
     ).order_by('-created_at')
+
+
+class ProfileDetailView(generics.RetrieveUpdateAPIView):
+    serializer_class = ProfileSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Profile.objects.all()
