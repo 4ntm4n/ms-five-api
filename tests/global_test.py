6c7dc181d16e68bc0969dde11c4da7565bbf2bcc
Anton Askling <anton.askling@gmail.com>
2023-02-27 00:46:56 +0100

    refactor user logout test. remove user endpoint test.

--- tests/global_test.py
+++ tests/global_test.py
@@ -10,6 +10,7 @@ class AuthTests(APITestCase):
         "signup": "/dj-rest-auth/registration/",
         "login": "/dj-rest-auth/login/",
         "logout": "/dj-rest-auth/logout/",
+        "user": "/dj-rest-auth/user/",
     }
 
     # object containig new user data and existing user data
@@ -67,12 +68,29 @@ class AuthTests(APITestCase):
 
     def test_if_user_can_logout(self):
         """
-        test if user can logout. expect: response.data to contain Successful logout message,
-        expect response status code to be 200.
+        1.Test if user is loged in is_auth to be true since last test.
+        2.Test if user can reach logout endpoind. expect postrequest 200 OK and sucessmessage
+        3.Test again if user is loged in is_auth to be False since now loged out.
+
         """
-        url = self.endpoints["logout"]
+        # set the user object and user url
         data = self.get_existing_user()
+        user_url = self.endpoints["user"] 
+
+        #save authentication status on current user, anonymos or loged in
+        is_auth = response.wsgi_request.user.is_authenticated
+        
+        #check if user is authenticated from prev test.
+        response = self.client.get(user_url, data, format='json')
+        self.assertTrue(is_auth)
+        
+        #check if user can reach logout endpoint and logout.
+        url = self.endpoints["logout"]
         response = self.client.post(url, data, format='json')
-        #self.assertNotIn("key", response.data)
         self.assertEqual(response.data["detail"], "Successfully logged out.")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        #check if user no longer is authenticated
+        response = self.client.get(user_url, data, format='json')
+        is_auth = response.wsgi_request.user.is_authenticated
+        self.assertFalse(is_auth)
