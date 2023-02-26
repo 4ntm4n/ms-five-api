828d651cc1d73495d94fe0210be1e2729faded2e
Anton Askling <anton.askling@gmail.com>
2023-02-26 16:45:37 +0100

    test if user exist. -pass. add test comments.

--- tests/global_test.py
+++ tests/global_test.py
@@ -6,6 +6,12 @@ from rest_framework.test import APITestCase
 class SignupTest(APITestCase):
 
     def setUp(self):
+        """
+        sets up a test version of the database,
+        creates a url to the registration endpoint, 
+        creats user data and then tries to post it to the
+        registration endpoind url using json format.  
+        """
         url = '/dj-rest-auth/registration/'
         data = {
             "username": "testUser",
@@ -16,8 +22,18 @@ class SignupTest(APITestCase):
 
     def test_if_signup_url_is_working(self):
         """
-        tests if a user can be created. 204 == success, 
-        since this endpoint will not return a any data, just add a user.
+        tests if a user can be created. 204 is a  success since 
+        this endpoint will not return a any data, just add a user.
         """
         self.assertEqual(self.response.status_code, status.HTTP_204_NO_CONTENT)
 
+    def test_if_user_exists_in_database(self):
+        """
+        here we test if the added user exists by saving the test user to a user variable
+        if the user is not created, the user will be None and test will fail
+        if user do exist, the user.username will be testUser and the test will pass.
+        """
+        user = User.objects.filter(username="testUser").first()
+        self.assertEqual(user.username, 'testUser')
+
+    
