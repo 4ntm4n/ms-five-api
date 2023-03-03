ab66aab80576a8d4b4a91b0d02160d59102b79a0
Anton Askling <anton.askling@gmail.com>
2023-03-03 19:37:44 +0100

    update permission class IsMember to NoOwnerAndMemberOrOwner.

--- rest_api/permissions.py
+++ rest_api/permissions.py
@@ -14,9 +14,29 @@ class IsGroupOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.group_owner == request.user.profile
 
-class IsMember(permissions.BasePermission):
+class NoOwnerAndMemberOrOwner(permissions.BasePermission):
     """
-    Custom permission to only allow members of a group associated with the task to view it.
+    Custom permission class, Gives permission if request.user.profile is 
+    member of group.members and task.owner is None. permission is also granted for
+    task.owner if a task owner is set.
     """
     def has_object_permission(self, request, view, obj):
-        return request.user.profile in obj.owning_group.members.all()
+
+        # check if user is not member of the group
+        if request.user.profile not in obj.owning_group.members.all():
+            return False
+
+        # check if task owner is NOT set to None and task owner is not the reqeusting user.
+        if obj.owner is not None and obj.owner != request.user:
+            return False
+
+        # check if member of the task's group and the request is get(read), grant access.
+        if request.method == 'GET' and request.user.profile in obj.owning_group.members.all():
+            return True
+
+        # if logic makes it here, it means that the requesting user IS in fact 
+        # the owner of the task OR that the object has no owner and requesting 
+        # user is a member of the task's owning group. 
+        # And in that case he or she shall be granted priveliges to get put patch 
+        # delete anything related to the task :). So we return True....
+        return True
