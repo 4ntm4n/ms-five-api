62b75dc9800e810547aef773b09a45b1f98248f0
Anton Askling <anton.askling@gmail.com>
2023-03-04 03:48:58 +0100

    add extra field for group name in task object

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -9,25 +9,37 @@ class TaskSerializer(serializers.ModelSerializer):
     in_progress = serializers.BooleanField()
     completed = serializers.BooleanField()
     owning_group = serializers.PrimaryKeyRelatedField(queryset=Group.objects.all())
-
+    group_name = serializers.SerializerMethodField()
+    #owner_username = serializers.SerializerMethodField()
+    
+    def get_group_name(self, obj):
+        if obj.owning_group.name:
+            return obj.owning_group.name
+        return None
+    
     class Meta:
         model = Task
         fields = [
             'id', 'title', 'description', 'owning_group', 'owner',
-            'in_progress', 'completed', 'created_at', 'updated_at',
+            'in_progress', 'completed', 'created_at', 'updated_at', 'group_name'
         ]
 
     def update(self, instance, validated_data):
+        profile = self.context['request'].user.profile
         completed = validated_data.get('completed', instance.completed)
         in_progress = validated_data.get('in_progress', instance.in_progress)
+        owner = validated_data.get('owner', instance.owner)
         
+        if owner and owner != profile:
+            raise serializers.ValidationError("you can not deligate tasks to other users...")
 
         instance.completed = completed
         if completed:
             instance.in_progress = False
         else:
             instance.in_progress = in_progress
-
+        
+        instance.owner = owner
         instance.save()
         return instance
     
