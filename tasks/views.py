1bc4c81ea42f0f2b3864ac4034207907b92958f2
Anton Askling <anton.askling@gmail.com>
2023-03-06 02:54:10 +0100

    fix: import http404 remove request

--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,13 @@
 from rest_framework import generics, request
 from django_filters.rest_framework import DjangoFilterBackend
-from django.http import request
+from django.http import Http404
 from .models import Task
 from groups.models import Group
 from .serializers import TaskSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import filters
 from django.db.models import Q
+
 
 
 class TaskListView(generics.ListAPIView):
