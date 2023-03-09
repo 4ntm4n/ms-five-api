41495d8a05db76352cbf2ade7578a1a2e1be5c67
Anton Askling <anton.askling@gmail.com>
2023-03-09 04:01:21 +0100

    add corsheaders middleware, and as installed app, add port 3000 to allowed origins

--- rest_api/settings.py
+++ rest_api/settings.py
@@ -67,7 +67,7 @@ DEBUG = 'DEV' in os.environ
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.environ.get('ALLOWED_HOSTS')]
 
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOWED_ORIGINS = ["http://127.0.0.1",]
+CORS_ALLOWED_ORIGINS = ["http://127.0.0.1", "http://localhost:3000"]
 CORS_ALLOW_CREDENTIALS = True
 
 # Application definition
@@ -85,6 +85,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'dj_rest_auth',
     'django_filters',
+    'corsheaders',
 
     #auth
     'django.contrib.sites',
@@ -103,6 +104,10 @@ SITE_ID = 1
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    #corsheaders middleware
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    #end corsheaders middleware
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
