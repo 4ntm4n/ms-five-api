a3edf018cf33438212d2ddc67fc2e3d43a16674e
Anton Askling <anton.askling@gmail.com>
2023-04-30 16:04:20 +0200

    set jwt rotate_refresh_token to true, install simpleJWT blacklist app

--- rest_api/settings.py
+++ rest_api/settings.py
@@ -46,7 +46,7 @@ if 'DEV' not in os.environ:
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
-    "ROTATE_REFRESH_TOKENS": False,
+    "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
 
@@ -124,6 +124,7 @@ INSTALLED_APPS = [
     'corsheaders',
 
     # auth
+    'rest_framework_simplejwt.token_blacklist',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
