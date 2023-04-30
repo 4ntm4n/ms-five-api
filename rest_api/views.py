cf72faad1642cdbfd629b70c95c61cbca00dbf2c
Anton Askling <anton.askling@gmail.com>
2023-04-30 16:08:50 +0200

    add custom serializer view to views.py

--- rest_api/views.py
+++ rest_api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import MyTokenObtainPairSerializer
 
 @api_view()
 def root_route(request):
@@ -8,3 +9,6 @@ def root_route(request):
         "message": "Welcome to the backend"
     })
 
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
